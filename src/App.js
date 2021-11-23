916d5c9686848ca2d7bdd045cd6ad699bd17514b
Prince Yekpahi <princeyekpahi@hotmail.fr>
2021-11-23 01:10:18 +0100

    Drawer

--- src/App.js
+++ src/App.js
@@ -1,65 +1,49 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-//import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { useWindowDimensions } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
 
-const Tab = createMaterialBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
+const App = () => {
 
-function App() {
- 
-    return (
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, size, color }) => {
-              let iconName;
-              if (route.name === 'Screen_A') {
-                iconName = 'autoprefixer';
-                size = focused ? 25 : 20;
-                // color = focused ? '#f0f' : '#555';
-              } else if (route.name === 'Screen_B') {
-                iconName = 'btc';
-                size = focused ? 25 : 20;
-                // color = focused ? '#f0f' : '#555';
-              }
-              return (
-                <FontAwesome5
-                  name={iconName}
-                  size={size}
-                  color={color}
-                />
-              )
-            }
-          })}
-          tabBarOptions={{
-            activeTintColor: '#f0f',
-            inactiveTintColor: '#555',
-            activeBackgroundColor: '#fff',
-            inactiveBackgroundColor: '#999',
-            showLabel: true,
-            labelStyle: { fontSize: 14 },
-            showIcon: true,
-          }}
-          activeColor='#f0edf6'
-          inactiveColor='#3e2465'
-          barStyle={{ backgroundColor: '#694fad' }}
-        >
-          <Tab.Screen
-            name="Screen_A"
-            component={ScreenA}
-            options={{ tabBarBadge: 3 }}
-          />
-          <Tab.Screen
-            name="Screen_B"
-            component={ScreenB}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    )
-  }
+  const dimensions = useWindowDimensions();
+  const isLargeScreen = dimensions.width >= 768;
+  return (
+    <NavigationContainer>
+    <Drawer.Navigator
+    initialRouteName="Screen_A"
+    drawerPosition='left'
+    drawerType="front"
+    edgeWidth={50}
+    hideStatusBar={false}
+    overlayColor='#00000090'
+    drawerStyle={{
+      backgroundColor: 'red',
+      width: 150
+    }}
+    screenOptions={{
+      headerShown: false,
+      swipeEnabled: true,
+      gestureEnabled: true,
+      headerTitleAlign: 'center',
+      headerStyle: {
+        backgroundColor: '#0080ff'
+      },
+      headerTintColor: '#ffffff',
+      headerTitleStyle: {
+        fontSize: 25,
+        fontWeight: 'bold'
+      }
+    }}
+    >
+      <Drawer.Screen name="Screen_A" component={ScreenA} />
+      <Drawer.Screen name="Screen_B" component={ScreenB} />
+    </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
 export default App;
